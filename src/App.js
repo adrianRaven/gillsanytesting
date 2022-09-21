1acd9f6f71806f5af86c7cf821d73f5712a19cd8
Adrian Alvarado Fernandez <dev.adrian.alvrado@gmail.com>
2022-09-20 20:18:39 -0500

    Revert "segundo commit de prueba"
    This reverts commit 304dd973d95a987c5af78495defda59e6ead0102.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Gillsany</p>
+        <p>Gillsany Sitio Oficial 2</p>
       </header>
     </div>
   );
