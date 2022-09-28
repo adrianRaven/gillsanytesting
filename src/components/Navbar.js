3151dfc7c84cdc250a5d8d9386e7b295475a724d
Adrian Alvarado Fernandez <dev.adrian.alvrado@gmail.com>
2022-09-28 11:12:23 -0500

    Navbar responsive etapa 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,34 @@
 import "../css/Navbar.css";
+import { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faChevronDown,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   return (
     <div className="navbar-contenedor">
-      <div className="navbar-top-section">
-        <div className="logo__nav">
-          <a href="/">
-            {" "}
-            <img
-              src={require("../img/logo_3.png")}
-              alt="Gillsany"
-              className="logo"
-            />
-          </a>
-        </div>
-      </div>
-      <div className="navbar-bottom-section">
-        <span className="nav-bar" id="btnMenu">
-          <FontAwesomeIcon icon={faBars} />
-        </span>
-
-        <div className="navbar-meu">
-          <ul className="menu" id="menu">
-            <li className="menu__item container__submenu">
-              <a href="/" className="menu__link submenu-btn">
-                Categorias <FontAwesomeIcon icon={faChevronDown} />
-              </a>
-              <ul className="submenu">
-                <li className="menu__item">
-                  <a href="/" className="menu__link">
-                    Ultrasonidos
-                  </a>{" "}
-                </li>
-                <li className="menu__item">
-                  <a href="/" className="menu__link">
-                    Electrocauterios
-                  </a>{" "}
-                </li>
-                <li className="menu__item">
-                  <a href="/" className="menu__link">
-                    Accesorios
-                  </a>{" "}
-                </li>
-              </ul>
-            </li>
-            <li className="menu__item">
-              <a href="/" className="menu__link">
-                Ofertas
-              </a>
-            </li>
-            <li className="menu__item">
-              <a href="/" className="menu__link">
-                Gillsany
-              </a>
-            </li>
-          </ul>
-
-          <div>
-            <ul className="menu">
-              <li className="menu__item">
-                <a href="/" className="menu__link">
-                  Carrito
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <h3 className="h3-nav">GILLSANY</h3>
+      <nav ref={navRef}>
+        <a href="/">Inicio</a>
+        <a href="/">Productos</a>
+        <a href="/">Ofertas</a>
+        <a href="/">Acerca de</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FontAwesomeIcon icon={faXmark} />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+      </button>
     </div>
   );
 }
