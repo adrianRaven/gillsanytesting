04eb757569ad1fd693ef4a61747d8de50c87c98b
Adrian Alvarado Fernandez <dev.adrian.alvrado@gmail.com>
2023-01-02 11:55:08 -0600

    countdown update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -64,7 +64,7 @@ function HomeScreen() {
               {" "}
               Próximo <span>Lanzamiento</span>
             </h1>
-            <p className="txtCustom">01 de Enero 2023</p>
+            <p className="txtCustom">13 de Enero 2023</p>
             <div className="launch-time">
               <div>
                 <p id="days">00</p>
