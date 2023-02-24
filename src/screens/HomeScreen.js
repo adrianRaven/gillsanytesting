93529d370292ab380287e0fafcbc42375ecae01f
Adrian Alvarado Fernandez <dev.adrian.alvrado@gmail.com>
2023-02-24 11:17:19 -0600

    commit to test digitalocean start

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -57,7 +57,7 @@ function HomeScreen() {
             <Product products={products.slice(0, 12)} />
           </>
         )}{" "}
-        <div className="title-section-two">Ofertas Mensuales</div>{" "}
+        <div className="title-section-two">Ofertas</div>{" "}
         {loading ? (
           <LoadingBox></LoadingBox>
         ) : error ? (
